06d52a86b68bbe55047f52dcf3ba78a4e573db25
xiong19971207 <278720216@qq.com>
2021-01-28 16:53:45 +0800

    主脚本文件提交

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# Program:
+#       这个程序输出
+# History:
+# 2020/01/28   bear     
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:~/bin
+export PATH
+
